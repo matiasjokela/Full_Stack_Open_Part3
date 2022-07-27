227f37028011b84da38e24f7e50dc0f539f4844b
matiasjokela <matias.jokela@outlook.com>
2022-07-27 18:44:21 +0300

    Up to 3.8 done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+morgan.token('body', (req, res) => {
+	if (req.method === 'POST')
+	{
+		return JSON.stringify(req.body)
+	} else {
+		return ' '
+	}
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 let persons = [
